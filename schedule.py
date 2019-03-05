d46693239afbe58d4f1af4d3bf4a74cef236f3e9
xachman <zironside@hotmail.com>
2019-03-05 12:34:34 -0500

    deal with time zones

--- schedule.py
+++ schedule.py
@@ -3,12 +3,15 @@ from googleapiclient.discovery import build
 import logger
 from datetime import datetime
 import time
+import os
+
 
 SPREADSHEET_ID = "1Jb_O1CtFU7l20hUi0dmUy_7nPVU0dqH0N89svBH-uIg"
 SHEET_ID="0"
 RANGE_NAME = 'Sheet1!A2:E'
 
 def getToday():
+    os.environ['TZ'] = 'US/Eastern'
     c = creds.get()
     service = build('sheets', 'v4', credentials=c)
 
@@ -21,14 +24,13 @@ def getToday():
     if not values:
         logger.log('No data found.')
     else:
+        currentDate = datetime.now()
         for row in values:
             # Print columns A and E, which correspond to indices 0 and 4.
-            currentDate = datetime.now()
-            startDate = datetime.strptime(row[0], "%Y-%m-%d").date()
-            endDate = datetime.strptime(row[1], "%Y-%m-%d").date()
-            logger.log(currentDate)
-            logger.log(startDate)
-            logger.log(endDate)
+            startDate = datetime.strptime(row[0], "%Y-%m-%d")
+            endDate = datetime.strptime(row[1]+" 23:59:59", "%Y-%m-%d %H:%M:%S")
+            logger.log(time.mktime(startDate.timetuple()))
+            logger.log(time.mktime(endDate.timetuple()))
 
             if currentDate.timestamp() > time.mktime(startDate.timetuple()) and currentDate.timestamp() < time.mktime(endDate.timetuple()):
                 return {"group": row[2], "type": row[3]}
